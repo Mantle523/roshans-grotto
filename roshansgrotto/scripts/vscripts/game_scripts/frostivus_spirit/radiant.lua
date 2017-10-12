b2f326b6f0db7ea68b6106d11623d9861c2868de
Mantle523 <32135754+Mantle523@users.noreply.github.com>
2017-10-13 00:31:19 +0100

    Add files via upload

---
+++ roshansgrotto/scripts/vscripts/game_scripts/frostivus_spirit/radiant.lua
@@ -0,0 +1,34 @@
+function InitialStacks(keys)
+	
+	local ability = keys.ability
+	local caster = keys.caster
+	local target = keys.target
+	local modifier = "modifier_frostivus_spirit_radiant"
+
+	local target_level = target:GetLevel() 
+
+	target:SetModifierStackCount(modifier, ability, target_level)
+end
+
+function RemoveStacks( keys )
+
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	local modifier = "modifier_frostivus_spirit_radiant"
+	local stacks = target:GetModifierStackCount(modifier, ability)
+
+	if target:HasModifier(modifier) then
+		target:SetModifierStackCount(modifier, ability, stacks - 1)
+		stacks = target:GetModifierStackCount(modifier, ability)
+
+		if stacks == 0 then
+			target:RemoveModifierByName(modifier)
+			ability:ApplyDataDrivenModifier(caster, target, "modifier_rg_pacifier_good", nil) 
+		end
+	
+		else if ability:ApplyDataDrivenModifier(caster, target, "modifier_rg_pacifier_bad", nil) then
+		end
+	end
+end
